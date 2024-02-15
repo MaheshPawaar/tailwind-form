dbd4bfa5fbc8fbb39b779637d245c75708ec5f95
Mahesh Pawar <pawarmahesh723@gmail.com>
2024-02-16 00:40:07 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        'black-dark':'#00000050',
+        'dull-white':'#ffffffb3',
+        'white-light':'#ffffff30',
+        'white-medium':'#ffffff40',
+        'neon-blue':'#2fb8ff',
+      }
+    },
+  },
+  plugins: [],
+}
+
